41d3adce52ae360b34eb8bc6d44828096cd83de2
Tobias Molina <102197218+TJMolina@users.noreply.github.com>
2023-08-30 09:08:16 -0300

    Add files via upload

---
+++ 30-8/1/main.js
@@ -0,0 +1,5 @@
+const temperatura = [24.5,11.2,5.6,7.8,3.4,2.4,10.8,11.4,9.8,25.0,22.3]
+const cuadruple = temperatura.map(elementos => elementos * 4);
+
+console.log("Arreglo original de temperaturas:", temperatura);
+console.log("Nuevo arreglo con elementos cuádruples:", cuadruple);
---
+++ 30-8/2/main.js
@@ -0,0 +1,7 @@
+const temperatura = [24.5,11.2,5.6,7.8,3.4,2.4,10.8,11.4,9.8,25.0,22.3]
+const cuadruple = temperatura.map(elementos => elementos * 4);
+
+console.log("Arreglo original de temperaturas:", temperatura);
+console.log("Nuevo arreglo con elementos cuádruples:", cuadruple);
+const far = temperatura.map(elemento1 => (elemento1 * 9/5) + 32);
+console.log(far);
